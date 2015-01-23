1e86a112e963af6ee38dbc3ce3b45b57cc1645e5
crysthianophp <crysthianophp@gmail.com>
2015-01-23 00:20:07 -0300

    estudos angularjs

--- capitulo1/src/js/directives.js
+++ capitulo1/src/js/directives.js
@@ -8,5 +8,4 @@ angular.module("myApp.directives", [])
 				    "<input data-ng-model='name.first' />" +
 				    "<input data-ng-model='name.last' />"
 	};
-})
-.directive('teste', function(){});
+});
